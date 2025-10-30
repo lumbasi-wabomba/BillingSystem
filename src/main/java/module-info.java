f97ee13cd7c43a2051b37bbc88c5798076d4509b
lumbasi-wabomba <lumbasiwk@gmail.com>
2025-10-30 13:16:24 +0300

    added javafx controls to module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,8 @@ module com.system.billingsystem {
     requires javafx.controls;
     requires javafx.fxml;
 
-
+    opens com.system.billingsystem.controller to javafx.fxml;
     opens com.system.billingsystem to javafx.fxml;
+
     exports com.system.billingsystem;
 }
