003c084a6ce60b7f68058789a00f9f47fc24dd14
Neuroflare-ux <alwaysgrind36@gmail.com>
2025-11-01 11:00:04 +0300

    models

--- src/main/java/com/system/billingsystem/models/Customers.java
+++ src/main/java/com/system/billingsystem/models/Customers.java
@@ -1,5 +1,33 @@
 package com.system.billingsystem.models;
 
 public class Customers {
+    private String id;
+    private String name;
+    private String email;
+    private String phoneNumber;
+
+    public Customers(String id, String name, String email, String phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+
+    // Getters and setters
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
 
 }
