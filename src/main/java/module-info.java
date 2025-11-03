35664f1f6929b453d7540e555a85dc445e31e637
lumbasi-wabomba <lumbasiwk@gmail.com>
2025-11-04 00:08:43 +0300

    final backend update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,7 @@ module com.system.billingsystem {
     requires javafx.fxml;
     requires java.sql;
     requires java.desktop;
-    requires com.system.billingsystem;
+    //requires com.system.billingsystem;
 
     opens com.system.billingsystem.controller to javafx.fxml;
     opens com.system.billingsystem to javafx.fxml;
